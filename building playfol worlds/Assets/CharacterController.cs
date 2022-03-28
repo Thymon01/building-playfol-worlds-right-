679a0199b29771a4887f9583b30613a66239051e
Thymon van der Jagt <thymon.vanderjagt@student.hku.nl>
2022-03-28 16:14:55 +0200

    scene switch gefixt en sprite gefixt

--- building playfol worlds/Assets/CharacterController.cs
+++ building playfol worlds/Assets/CharacterController.cs
@@ -4,13 +4,18 @@ public class CharacterController : MonoBehaviour
 {
     public float MovementSpeed = 1;
     public float JumpForce = 1;
+    public Transform myLight;
 
     private Rigidbody2D _rigidbody;
+
+    private SpriteRenderer _sprite;
 
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        _sprite = GetComponent<SpriteRenderer>();
     }
+
 
 
     private void Update()
@@ -21,6 +26,22 @@ public class CharacterController : MonoBehaviour
         if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.001f)
         {
             _rigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+        }
+
+        if (movement > 0)
+        {
+            _sprite.flipX = false;
+            if (myLight.localScale.x < 0)
+            { myLight.localScale = Vector3.Scale(myLight.localScale, new Vector3(-1, 1, 1)); }
+        }
+        else
+        {
+            if (movement != 0)
+            {
+                _sprite.flipX = true;
+                if (myLight.localScale.x > 0)
+                { myLight.localScale = Vector3.Scale(myLight.localScale, new Vector3(-1, 1, 1)); }
+            }
         }
     }
 }
---
+++ building playfol worlds/Assets/SceneSwitchScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneSwitchScript : MonoBehaviour
+{
+   
+    public GameObject sceneSwitch;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        sceneSwitch.SetActive(true);
+        
+    }
+}
