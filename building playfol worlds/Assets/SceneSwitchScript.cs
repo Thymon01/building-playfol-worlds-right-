2d5e05d8073283716cce6dde3d845f85aac8feec
Thymon van der Jagt <thymon.vanderjagt@student.hku.nl>
2022-03-29 14:52:56 +0200

    scene switch verbeterd

--- building playfol worlds/Assets/SceneSwitchScript.cs
+++ building playfol worlds/Assets/SceneSwitchScript.cs
@@ -6,10 +6,15 @@ public class SceneSwitchScript : MonoBehaviour
 {
    
     public GameObject sceneSwitch;
+    public GameObject tikkiegroepred;
+    public GameObject normalTikkieGroep;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         sceneSwitch.SetActive(true);
-        
+        tikkiegroepred.SetActive(true);
+        normalTikkieGroep.SetActive(false);
+
+
     }
 }
